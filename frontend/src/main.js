0d6b7e8b597e4b915cbbfaf556564733f1d2f2fd
souhei <hanasouhei1211@gmail.com>
2021-08-23 13:47:28 +0900

    Update: axiosをインストール
      baseURLも設定

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,9 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
 import './plugins/element'
 
 Vue.config.productionTip = false
+
+Vue.prototype.$http = axios
+axios.defaults.baseURL = process.env.API_URL
 
 new Vue({
   router,
