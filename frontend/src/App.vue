34c36fb8c00e8c448df49d6d03f59eac0e76a8d9
souhei <hanasouhei1211@gmail.com>
2021-08-23 19:40:53 +0900

    Add: flashを作成:

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,21 @@
 <template>
   <div id="app">
+    <Header></Header>
+    <Flash></Flash>
     <router-view/>
   </div>
 </template>
 
 <script>
 import 'normalize.css' // reset.css
+import Header from './components/layouts/Header.vue'
+import Flash from './components/layouts/Flash.vue'
 
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    Header,
+    Flash
+  }
 }
 </script>
