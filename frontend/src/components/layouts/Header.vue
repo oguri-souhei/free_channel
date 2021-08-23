39b3e2ab2afe691f4f92ca49c8322648e636a8e1
souhei <hanasouhei1211@gmail.com>
2021-08-23 16:03:30 +0900

    Add: ヘッダーを作成

---
+++ frontend/src/components/layouts/Header.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="header">
+    <el-menu class="el-menu-demo" mode="horizontal" active-text-color="none">
+      <el-menu-item index="1">
+        <router-link to="/">FreeChannel</router-link>
+      </el-menu-item>
+      <el-menu-item index="2">
+        <el-input placeholder="ルームを検索"></el-input>
+      </el-menu-item>
+      <el-menu-item index="3">
+        <router-link to="#">
+          <el-button type="primary" round>アカウント登録</el-button>
+        </router-link>
+      </el-menu-item>
+      <el-menu-item index="4">
+        <router-link to="/login">
+          <el-button type="primary" round plain>ログイン</el-button>
+        </router-link>
+      </el-menu-item>
+    </el-menu>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style scoped>
+a {
+  text-decoration: none;
+}
+</style>
