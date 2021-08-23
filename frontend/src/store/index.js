2b35f44538566720fa370bcd8cdc5d4ea8b09c47
souhei <hanasouhei1211@gmail.com>
2021-08-23 17:46:31 +0900

    Update: vuexにログイン中のユーザーを保存

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -4,12 +4,39 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  strict: true,
   state: {
+    flash: {
+      msg: '', // flashのメッセージ
+      type: '' // flashのタイプ（success, warning, error)
+    },
+    currentUser: {
+      id: '',
+      name: '',
+      email: '',
+      created_at: '',
+      updated_at: ''
+    }
+  },
+  getters: {
+    isLoggedIn(state) {
+      return state.currentUser !== null ? true : false
+    }
   },
   mutations: {
+    setFlash(state, flash) {
+      state.flash = flash
+    },
+    setCurrentUser(state, user) {
+      state.currentUser = user
+    }
   },
   actions: {
-  },
-  modules: {
+    setFlash(context, flash) {
+      context.commit('setFlash', flash)
+    },
+    setCurrentUser(context, user) {
+      context.commit('setCurrentUser', user)
+    }
   }
 })
