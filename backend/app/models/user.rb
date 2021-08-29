be9bfe692e6b282726855a37c79cc75ce69979f7
souhei <hanasouhei1211@gmail.com>
2021-08-29 22:20:24 +0900

    Update: Users#update_without_passwordを作成

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -8,4 +8,15 @@ class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, length: { maximum: 250 },
                     format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
+
+  # パスワードが空でもユーザーを更新
+  def update_without_password(user_params)
+    if user_params[:password].blank? && user_params[:password_confirmation].blank?
+      user_params.delete(:password) && user_params.delete(:password_confirmation)
+    end
+
+    clean_up_passwords
+
+    update(user_params)
+  end
 end
--- backend/spec/models/user_spec.rb
+++ backend/spec/models/user_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   let(:user) { build(:user) }
+  let(:tom) { create(:tom) }
 
   describe 'Validation' do
     # 名前
@@ -57,6 +58,144 @@ RSpec.describe User, type: :model do
     # パスワード（確認用）
     context 'password_confirmation' do
       it { should validate_presence_of(:password_confirmation).allow_nil.with_message('を入力してください') }
+    end
+  end
+
+  describe 'Methods' do
+    context '#update_without_password' do
+      let(:user_params) { attributes_for(:user, password: '   ', password_confirmation: '     ') }
+      let(:user_params_with_password) { attributes_for(:user) }
+
+      context 'when success' do
+        it 'returns true' do
+          result = tom.update_without_password(user_params)
+          expect(result).to be_truthy
+        end
+
+        it 'updates user without password' do
+          tom.update_without_password(user_params)
+          aggregate_failures do
+            expect(tom.name).to eq user_params[:name]
+            expect(tom.email).to eq user_params[:email]
+            expect(user_params[:password]).to_not be_valid_password_of(tom)
+          end
+        end
+  
+        # パスワードありでも更新できる
+        it 'updates user with password' do
+          tom.update_without_password(user_params_with_password)
+          aggregate_failures do
+            expect(tom.name).to eq user_params_with_password[:name]
+            expect(tom.email).to eq user_params_with_password[:email]
+            expect(user_params_with_password[:password]).to be_valid_password_of(tom)
+          end
+        end
+      end
+
+      context 'when name is blank' do
+        let(:wrong_params) { attributes_for(:user, name: '  ') }
+
+        it 'returns false' do
+          result = tom.update_without_password(wrong_params)
+          expect(result).to be_falsey
+        end
+
+        it 'does not update user' do
+          tom.update_without_password(wrong_params)
+          tom.reload
+          expect(tom.name).to_not eq wrong_params[:name]
+        end
+      end
+
+      context 'when name is too long' do
+        let(:wrong_params) { attributes_for(:user, name: 'a' * 51) }
+
+        it 'returns false' do
+          result = tom.update_without_password(wrong_params)
+          expect(result).to be_falsey
+        end
+
+        it 'does not update user' do
+          tom.update_without_password(wrong_params)
+          tom.reload
+          expect(tom.name).to_not eq wrong_params[:name]
+        end
+      end
+
+      context 'when email is blank' do
+        let(:wrong_params) { attributes_for(:user, email: '    ') }
+
+        it 'returns false' do
+          result = tom.update_without_password(wrong_params)
+          expect(result).to be_falsey
+        end
+
+        it 'does not update user' do
+          tom.update_without_password(wrong_params)
+          tom.reload
+          expect(tom.email).to_not eq wrong_params[:email]
+        end
+      end
+
+      context 'when email is too long' do
+        let(:wrong_params) { attributes_for(:user, name: 'a' * 240 + '@example.com') }
+
+        it 'returns false' do
+          result = tom.update_without_password(wrong_params)
+          expect(result).to be_falsey
+        end
+
+        it 'does not update user' do
+          tom.update_without_password(wrong_params)
+          tom.reload
+          expect(tom.email).to_not eq wrong_params[:email]
+        end
+      end
+
+      context 'when email is invalid' do
+        let(:wrong_params) { attributes_for(:user, email: 'foo@bar,com') }
+
+        it 'returns false' do
+          result = tom.update_without_password(wrong_params)
+          expect(result).to be_falsey
+        end
+
+        it 'does not update user' do
+          tom.update_without_password(wrong_params)
+          tom.reload
+          expect(tom.email).to_not eq wrong_params[:email]
+        end
+      end
+
+      context 'when password is too short' do
+        let(:wrong_params) { attributes_for(:user, password: 'aaa') }
+
+        it 'returns false' do
+          result = tom.update_without_password(wrong_params)
+          expect(result).to be_falsey
+        end
+
+        it 'does not update user' do
+          tom.update_without_password(wrong_params)
+          tom.reload
+          expect(wrong_params[:password]).to_not be_valid_password_of(tom)
+        end
+      end
+
+      context 'when password_confirmation is wrong' do
+        let(:wrong_params) { attributes_for(:user, password: 'password', password_confirmation: 'foobar') }
+
+        it 'returns false' do
+          result = tom.update_without_password(wrong_params)
+          expect(result).to be_falsey
+        end
+
+        it 'does not update user' do
+          tom.update_without_password(wrong_params)
+          tom.reload
+          expect(wrong_params[:password]).to_not be_valid_password_of(tom)
+        end
+      end
     end
   end
 end
