e62555c45a8f5a729fc52041c6f23dc1257873a7
souhei <hanasouhei1211@gmail.com>
2021-09-17 15:03:51 +0900

    Modify: コメントを書いた

--- frontend/src/modules/date.js
+++ frontend/src/modules/date.js
@@ -1,4 +1,5 @@
 // railsのcreated_atなどの時間表記を変える
+// 2021-09-17T09:46:02.431+09:00 など
 export function dateTime (railsTime) {
   const reg = /^(\d{4})-(\d{2})-(\d{1,2})T/
   const matched = railsTime.match(reg)
