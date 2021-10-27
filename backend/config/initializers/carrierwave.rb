e15fdfd8b721aff6bbc95f0ac1db1ba3052742c2
souhei <hanasouhei1211@gmail.com>
2021-10-27 16:52:37 +0900

    Update: 画像をs3にアップロードするように設定

---
+++ backend/config/initializers/carrierwave.rb
@@ -0,0 +1,14 @@
+unless Rails.env.development? || Rails.env.test?
+  CarrierWave.configure do |config|
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+      region: ENV['AWS_REGION']
+    }
+
+    config.fog_directory  = 'rails'
+    config.cache_storage = :fog
+  end
+end
+
