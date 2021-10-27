450bf13772bcc45c390ae4f586052643822f6332
souhei <hanasouhei1211@gmail.com>
2021-10-27 23:43:22 +0900

    Fix: パスしなくなっていたテストを修正

--- frontend/src/components/users/Avatar.vue
+++ frontend/src/components/users/Avatar.vue
@@ -27,7 +27,7 @@ export default {
   },
   computed: {
     avatarUrl() {
-      return this.url
+      return process.env.NODE_ENV === 'production' ? this.url : process.env.VUE_APP_API_HOST + this.url
     }
   }
 }
